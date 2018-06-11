be2bb20b9f4c02548673683aee4d8240655032b7
Samir L. Boulema <sboulema@gmail.com>
2018-06-11 13:17:38 +0200

    Switch to AsyncPackage

--- TGit/TGITPackage.cs
+++ TGit/TGITPackage.cs
@@ -8,20 +8,25 @@ using Microsoft.VisualStudio.Shell.Interop;
 using SamirBoulema.TGit.Helpers;
 using SamirBoulema.TGit.Commands;
 using VsShell14 = vsshell14::Microsoft.VisualStudio.Shell;
+using SamirBoulema.TGit.Helpers.AsyncPackageHelpers;
+using System;
 
 namespace SamirBoulema.TGit
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [AsyncPackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    //[VsShell14.ProvideMenuResource("Menus.ctmenu", 1, IconMappingFilename = "IconMappings.csv")]
-    [VsShell14.ProvideMenuResource("Menus.ctmenu", 1)]
+    [VsShell14.ProvideMenuResource("Menus.ctmenu", 1, IconMappingFilename = "IconMappings.csv")]
+    //[VsShell14.ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.GuidTgitPkgString)]
-    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    [Helpers.AsyncPackageHelpers.ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideOptionPage(typeof(OptionPageGrid), "TGit", "General", 0, 0, true)]
-    public sealed class TGitPackage : Package
+    public sealed class TGitPackage : Package, IAsyncLoadablePackageInitialize
     {
         private DTE _dte;
         private SolutionEvents _events;
+        private bool isAsyncLoadSupported;
+        private OleMenuCommandService _mcs;
+        private OptionPageGrid _options;
 
         /// <summary>
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
@@ -31,22 +36,66 @@ namespace SamirBoulema.TGit
         {
             base.Initialize();
 
-            _dte = (DTE)GetService(typeof(DTE));        
-            var options = (OptionPageGrid)GetDialogPage(typeof(OptionPageGrid));
+            isAsyncLoadSupported = this.IsAsyncPackageSupported();
+
+            // Only perform initialization if async package framework is not supported
+            if (!isAsyncLoadSupported)
+            {
+                BackgroundThreadInitialization();
+                MainThreadInitialization();
+            }
+        }
+
+        /// <summary>
+        /// Performs the asynchronous initialization for the package in cases where IDE supports AsyncPackage.
+        /// 
+        /// This method is always called from background thread initially.
+        /// </summary>
+        /// <param name="asyncServiceProvider">Async service provider instance to query services asynchronously</param>
+        /// <param name="pProfferService">Async service proffer instance</param>
+        /// <param name="IAsyncProgressCallback">Progress callback instance</param>
+        /// <returns></returns>
+        public IVsTask Initialize(Microsoft.VisualStudio.Shell.Interop.IAsyncServiceProvider asyncServiceProvider, 
+            IProfferAsyncService pProfferService, IAsyncProgressCallback pProgressCallback)
+        {
+            if (!isAsyncLoadSupported)
+            {
+                throw new InvalidOperationException("Async Initialize method should not be called when async load is not supported.");
+            }
+
+            return ThreadHelper.JoinableTaskFactory.RunAsync<object>(async () =>
+            {
+                BackgroundThreadInitialization();
+
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                MainThreadInitialization();
+                return null;
+            }).AsVsTask();
+        }
+
+        private void BackgroundThreadInitialization()
+        {
+            _dte = (DTE)GetService(typeof(DTE));
+            _options = (OptionPageGrid)GetDialogPage(typeof(OptionPageGrid));
 
             _events = _dte.Events.SolutionEvents;
             _events.Opened += SolutionEvents_Opened;
             _events.AfterClosing += SolutionEvents_AfterClosing;
 
             // Add our command handlers for menu (commands must exist in the .vsct file)
-            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if (null == mcs) return;
+            _mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+        }
 
-            new MainMenuCommands(mcs, _dte, options).AddCommands();
+        private void MainThreadInitialization()
+        {
+            if (null == _mcs) return;
 
-            new ContextMenuCommands(mcs, _dte, options).AddCommands();
+            new MainMenuCommands(_mcs, _dte, _options).AddCommands();
 
-            new GitFlowMenuCommands(mcs, options).AddCommands();
+            new ContextMenuCommands(_mcs, _dte, _options).AddCommands();
+
+            new GitFlowMenuCommands(_mcs, _options).AddCommands();
 
             // Add all menus
             var tgitMenu = CommandHelper.CreateCommand(PkgCmdIDList.TGitMenu);
@@ -62,17 +111,17 @@ namespace SamirBoulema.TGit
                     tgitMenu.Text = "TGit";
                     tgitContextMenu.Text = "TGit";
                     break;
-            }       
-            mcs.AddCommand(tgitMenu);
-            mcs.AddCommand(tgitContextMenu);
+            }
+            _mcs.AddCommand(tgitMenu);
+            _mcs.AddCommand(tgitContextMenu);
 
             var tgitGitFlowMenu = CommandHelper.CreateCommand(PkgCmdIDList.TGitGitFlowMenu);
             tgitGitFlowMenu.BeforeQueryStatus += CommandHelper.SolutionVisibility_BeforeQueryStatus;
-            mcs.AddCommand(tgitGitFlowMenu);
+            _mcs.AddCommand(tgitGitFlowMenu);
 
             var tgitGitHubFlowMenu = CommandHelper.CreateCommand(PkgCmdIDList.TGitGitHubFlowMenu);
             tgitGitHubFlowMenu.BeforeQueryStatus += CommandHelper.GitHubFlow_BeforeQueryStatus;
-            mcs.AddCommand(tgitGitHubFlowMenu);
+            _mcs.AddCommand(tgitGitHubFlowMenu);
         }
 
         private static void SolutionEvents_AfterClosing()
